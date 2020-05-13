e096387ee37929a32e9cb9acbf55cdd07aed8b5c
Andi Dwi <61771890+andiads@users.noreply.github.com>
2020-05-13 18:35:30 +0700

    revert back to origin

--- src/app/components/update-user/update-user.component.ts
+++ src/app/components/update-user/update-user.component.ts
@@ -1,11 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../../models/user';
-import { Roles } from '../../models/roles';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UserService } from '../../services/user.service';
-import { AdminService } from '../../services/admin.service';
-import { Observable } from 'rxjs';
-import { FormBuilder, FormGroup, FormArray, FormControl, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'app-update-user',
@@ -14,27 +10,16 @@ import { FormBuilder, FormGroup, FormArray, FormControl, ValidatorFn } from '@an
 })
 export class UpdateUserComponent implements OnInit {
 
-  form: FormGroup;
   id: number;
   user: User;
-  roles: Observable<Roles[]>;
   submitted = false;
 
-  constructor(
-    private route: ActivatedRoute, 
-    private router: Router,
-    private formBuilder: FormBuilder,
-    private userService: UserService,
-    private adminService: AdminService
-  ) {
-    this.form = this.formBuilder.group({
-      roles: this.adminService.getRolesList()
-    });
-   }
+  constructor(private route: ActivatedRoute, private router: Router,
+    private userService: UserService) { }
 
   ngOnInit() {
-
     this.user = new User();
+
     this.id = this.route.snapshot.params['id'];
 
     this.userService.getUser(this.id)
@@ -44,19 +29,15 @@ export class UpdateUserComponent implements OnInit {
       }, error => console.log(error));
   }
 
-  loadListRoles() {
-    this.roles = this.adminService.getRolesList();
-  }
-
   updateUser() {
     this.userService.updateUser(this.id, this.user)
       .subscribe(data => console.log(data), error => console.log(error));
-    this.user = new User();
-    this.gotoList();
+      this.user = new User();
+      this.gotoList();
   }
 
   onSubmit() {
-    this.submitted = true;
+    this.submitted=true;
     this.updateUser();
   }
 
